d05cdad0a8016a1ded8d175185c3b11b5eea630d
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2023-08-11 17:47:47 +0000

    Finished app development

--- src/app.py
+++ src/app.py
@@ -1,4 +1,22 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+from pickle import load
 
-# your code here
+app = Flask(__name__)
+with open("/workspaces/flask_demo_project/models/logistic_default.sav", 'rb') as f:
+    model = load(f)
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    prediction = None
+    if request.method == "POST":
+        # Tengo que coger los datos que el usuario ha introducido ANTES de pulsar al botón de Predecir
+        Pclass = float(request.form["val1"])
+        Fare = float(request.form["val2"])
+        Sex_n = float(request.form["val3"])
+        Embarked_n = float(request.form["val4"])
+        FamMembers = float(request.form["val5"])
+
+        data_a_predecir = [[Pclass, Fare, Sex_n, Embarked_n, FamMembers]]
+        prediction = model.predict(data_a_predecir)
+
+    return render_template("index.html", prediction = prediction)
